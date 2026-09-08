2292c14aedf359c6c95a7135f672e3d1ad4c248e
dmnmsc <dmnmsc@users.noreply.github.com>
2026-09-08 22:00:23 +0200

    Revert "Add 'morgue' directory (WhatsApp) to CLEAN_DIRS"
    This reverts commit e3117eb82bc7acd958c02681d5ca89ae26229f8a.

--- pwaclean.py
+++ pwaclean.py
@@ -20,8 +20,7 @@ DEFAULT_PROFILE_ULID = "00000000000000000000000000"
 # User can add, remove, or modify directories here to customize cleaning.
 CLEAN_DIRS = [
     "cache2", "startupCache", "offlineCache", "jumpListCache",
-    "minidumps", "saved-telemetry-pings", "datareporting",
-    "morgue"
+    "minidumps", "saved-telemetry-pings", "datareporting"
 ]
 
 # User can set custom paths here if autodetect fails or if installed elsewhere.
